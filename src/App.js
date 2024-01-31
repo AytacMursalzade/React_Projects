d3f37f3488d37f87ca2d5c0a6cb12c19877ba227
Aytac <aytacmurselzade381@gmail.com>
2024-02-01 02:55:42 +0400

    Upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Todo from "./Components/ToDoList/Todo.js";
-import {BrowserRouter , Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RandomProfile from "./Components/RandomProfileGenerator/RandomProfile.js";
 import AccordionMenu from "./Components/AccordionMenu/AccordionMenu.js";
-import IncDec from './Components/IncDec/IncDec.js';
-import Quote from './Components/Quote/Quote';
+import IncDec from "./Components/IncDec/IncDec.js";
+import Quote from "./Components/Quote/Quote";
+import Menu from "./Components/Menu/Menu.js";
 
 function App() {
-
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/todo" element={<Todo />} />
-        <Route path="/incdec" element={<IncDec />} />
-        <Route path="/randomprofile" element={<RandomProfile />} />
-        <Route path="/accordionmenu" element={<AccordionMenu />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
-    </BrowserRouter>  
-  )
+  return (
+    <>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route  index element={<Menu />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/incdec" element={<IncDec />} />
+          <Route path="/randomprofile" element={<RandomProfile />} />
+          <Route path="/accordionmenu" element={<AccordionMenu />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cardapp from './Cardapp'
+
+function Menu() {
+  return (
+    <div className='bg-[#887A82] h-screen  grid  grid-cols-5 pt-[70px] pl-[80px] '>
+      <Cardapp />
+      <Cardapp />
+      <Cardapp />
+      <Cardapp />
+      <Cardapp />
+    
+    </div>
+  )
+}
+
+export default Menu
