c1aa9f4fdc99e274dac8300ac5830bdb35c38a31
Aytac <aytacmurselzade381@gmail.com>
2024-02-18 23:29:44 +0400

    accordionmenu

---
+++ src/Components/AccordionMenu/Question.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+function Question({ header, text }) {
+  const [showDetail, setShowDetail] = useState(false);
+
+  return (
+    <div className="flex gap-[50px] ">
+      <div className="w-[380px] ">
+        <h1>{text}</h1>
+      </div>
+      <div>
+        <button onClick={() => setShowDetail(!showDetail)}>
+          {showDetail ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </div>
+      {showDetail && <p>{header}</p>}
+    </div>
+  );
+}
+
+export default Question;
