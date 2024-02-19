1df58d444919dfd048d3fa64f06bfd009217089e
Aytac <aytacmurselzade381@gmail.com>
2024-02-19 20:35:38 +0400

    UpdateChange

--- src/Components/AccordionMenu/AccordionMenu.js
+++ src/Components/AccordionMenu/AccordionMenu.js
@@ -6,16 +6,19 @@ function AccordionMenu() {
   const [questions, setQuestions] = useState(data);
 
   return (
-    <div className="Place h-screen bg-[#c0b1b9] flex flex-col gap-[50px] justify-center items-center">
-      <div className="PlaceH  ">
-        <h1>Accordion Menu</h1>
-      </div>
-      <div>
-        <section className="PlaceList ">
-          {questions.map((item) => {
-            return <Question key={item.id} {...item} />;
-          })}
-        </section>
+    <div className="bg-[#c0b1b9] h-screen">
+      
+      <div className="Place flex flex-col gap-[10px] justify-center items-center">
+        <div className="PlaceH pt-[80px] ">
+          <h1>Accordion Menu</h1>
+        </div>
+        <div className="pt-[10%]">
+          <section className="PlaceList ">
+            {questions.map((item) => {
+              return <Question key={item.id} {...item} />;
+            })}
+          </section>
+        </div>
       </div>
     </div>
   );
